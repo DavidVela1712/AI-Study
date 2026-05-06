c7ddeb8f9c27ac577b9d9c2f793f80c9d1fc5912
David <davidvela1712@gmail.com>
2026-05-06 19:36:58 +0200

    Inicio de proyecto

--- backend/src/main/java/com/app/aistudy/AiStudyApplication.java
+++ backend/src/main/java/com/app/aistudy/AiStudyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiStudyApplication {
+    //prueba
 
     public static void main(String[] args) {
         SpringApplication.run(AiStudyApplication.class, args);
