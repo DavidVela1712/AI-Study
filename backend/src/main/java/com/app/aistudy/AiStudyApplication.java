742599946c4c70d496d0fa97cee5462356fe02d2
David <davidvela1712@gmail.com>
2026-05-07 20:11:39 +0200

    Añadido frontend

--- backend/src/main/java/com/app/aistudy/AiStudyApplication.java
+++ backend/src/main/java/com/app/aistudy/AiStudyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiStudyApplication {
-    //prueba
 
     public static void main(String[] args) {
         SpringApplication.run(AiStudyApplication.class, args);
